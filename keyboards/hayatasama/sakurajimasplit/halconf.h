85c588e934cb3218cbe020ad9c30bb9ae55cb4c0
HayataSama <amir.khateri135@gmail.com>
2025-10-31 10:03:04 +0330

    add SakurajimaSplit keyboard

---
+++ keyboards/hayatasama/sakurajimasplit/halconf.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define HAL_USE_SERIAL TRUE // split comm
+
+#include_next <halconf.h>
---
+++ keyboards/hayatasama/sakurajimasplit/rules.mk
@@ -0,0 +1,8 @@
+# Enable debug information in the final binaries
+DEBUG_ENABLE = yes
+# Disable optimisations for debugging purposes
+LTO_ENABLE = no
+OPT = g
+
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = usart
